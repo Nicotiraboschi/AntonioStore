57e25763845ff3016795e97acbd1cdcd7e36a1cc
Nicotiraboschi <nico.tiraboschi@gmail.com>
2023-09-13 13:06:09 +0200

    small changes, commit 1

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("d928d33c-fc4a-4500-9b3b-9a489ba74a71");
 
   return (
     <Container>
